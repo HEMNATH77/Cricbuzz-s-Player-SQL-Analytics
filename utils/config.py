36f271180552ed76ae156296bcde4bc517cf25e0
Hemnath M <146712643+HEMNATH77@users.noreply.github.com>
2025-10-03 10:33:07 +0530

    Add files via upload

---
+++ utils/config.py
@@ -0,0 +1,12 @@
+# config.py
+
+HEADERS = {
+    "x-rapidapi-key": "d8ce6d3122mshd84847e1f215325p1db145jsnc5ecec004cfa",
+    "x-rapidapi-host": "cricbuzz-cricket.p.rapidapi.com"
+}
+
+PLAYER_SEARCH_URL = "https://cricbuzz-cricket.p.rapidapi.com/stats/v1/player/search"
+PLAYER_STATS_URL = "https://cricbuzz-cricket.p.rapidapi.com/stats/v1/player/{player_id}"
+PLAYER_BATTING_URL = "https://cricbuzz-cricket.p.rapidapi.com/stats/v1/player/{player_id}/batting"
+PLAYER_BOWLING_URL = "https://cricbuzz-cricket.p.rapidapi.com/stats/v1/player/{player_id}/bowling"
+
